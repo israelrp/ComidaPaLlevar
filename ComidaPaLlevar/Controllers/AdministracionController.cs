85015346fd925f1d7fa253bf16419d751ca082d7
israelrp <israel@novatek.me>
2014-04-29 00:56:57 -0500

    Index corregido

---
+++ ComidaPaLlevar/Controllers/AdministracionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComidaPaLlevar.Controllers
+{
+    public class AdministracionController : Controller
+    {
+        //
+        // GET: /Administracion/
+        public ActionResult Clientes()
+        {
+            return View();
+        }
+
+        public ActionResult Menus()
+        {
+            return View();
+        }
+
+        public ActionResult Ordenes()
+        {
+            return View();
+        }
+	}
+}
